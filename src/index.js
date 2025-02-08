f275e87a45ff863463832510b1611e38b479d44b
defjunior <67216590+defjunior@users.noreply.github.com>
2025-02-08 15:28:55 -0500

    routing

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Searchbar from './Searchbar';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Searchbar />
     <App />
   </React.StrictMode>
 );
