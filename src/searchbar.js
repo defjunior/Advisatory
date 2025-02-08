e0d2aa87f40613bea0d51e100cc0f84a942bc29a
defjunior <67216590+defjunior@users.noreply.github.com>
2025-02-08 14:55:12 -0500

    searchbar

---
+++ src/searchbar.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+import './searchbar.css';
+
+function searchbar() {
+
+
+
+    return (
+
+        <>
+        <div>
+            
+        </div>
+        </>
+    )
+}
+export default searchbar
