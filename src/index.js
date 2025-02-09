36a813ffd779871a3c731a6ee84ce1642d1a2208
defjunior <67216590+defjunior@users.noreply.github.com>
2025-02-08 19:46:12 -0500

    route

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
 
